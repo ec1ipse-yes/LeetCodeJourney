b74c9bee13964c70db8f409604b5dfe9bd3970e9
ec1ipse-yes <eclipse.tenet@gmail.com>
2021-06-24 23:47:40 +0800

    Update problems; 2021/06/24

--- src/637.AverageofLevelsinBinaryTree/solution.cpp
+++ src/637.AverageofLevelsinBinaryTree/solution.cpp
@@ -36,3 +36,27 @@ private:
 };
 
 // Solution2 -> BDS: Breadth First Search
+class Solution2 {
+public:
+    vector<double> averageOfLevels(TreeNode* root) {
+        if (root == nullptr) return {};
+        // Initialization.
+        vector<double> res;
+        vector<TreeNode*> curr, next;
+        curr.push_back(root);
+        // Process every level one by one.
+        while (!curr.empty()) {
+            long long sum = 0;
+            for (const auto& node : curr) {
+                sum += node->val;
+                if (node->left) next.push_back(node->left);
+                if (node->right) next.push_back(node->right);
+            }
+
+            res.push_back(static_cast<double>(sum) / curr.size());
+            curr.swap(next);
+            next.clear();
+        }
+        return res;
+    }
+}
